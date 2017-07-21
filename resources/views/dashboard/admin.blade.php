8b4db0ed1fa809b0c9c2e0a7270d1a91b21f7de9
151610254-smkassalambandung <151610254@smkassalambandung.sch.id>
2017-07-21 11:14:30 +0700

    getjobs setengah jadi

---
+++ resources/views/dashboard/admin.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Dashboard</h2>
+                </div>
+
+                <div class="panel-body">
+                    <center><h2>Selamat Datang Di Menu GetJobs.com. Silahkan Pilih Menu yang diinginkan.</h2></center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <center><h1>Selamat Datang Di GetJobs.com</h1></center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
